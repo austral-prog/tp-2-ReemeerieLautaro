502f6b5979ad3242de2a5ed32d4a0b96a1cf67f8
ReemeerieLautaro <lreemeerie@mail.austral.edu.ar>
2025-04-03 14:24:32 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,11 @@
 def change():
     expense = 23.75
     money = 100
+    vuelto = money - expense
+    
+    print(f"{"Ingresar gastos:"} {expense}")
+    print(f"{"Dinero recibido:"} {money}")
+    print(int(vuelto))
+    print(float(vuelto) - int(vuelto))
+
+change()
