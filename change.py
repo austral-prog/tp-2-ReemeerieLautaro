747f811f7ce1dbcdee730afbc7dfb47ec61fbaf9
ReemeerieLautaro <lreemeerie@mail.austral.edu.ar>
2025-04-04 11:35:34 -0300

    Update change.py

--- change.py
+++ change.py
@@ -3,9 +3,10 @@ def change():
     money = 100
     vuelto = money - expense
     
-    print(f"{"Ingresar gastos:"} {expense}")
-    print(f"{"Dinero recibido:"} {money}")
-    print(int(vuelto))
-    print(float(vuelto) - int(vuelto))
+    print(f"{"Ingresar gastos:"} \n{expense}")
+    print(f"{"Dinero recibido:"} \n{money}")
+    print ("Vuelto")
+    print(f"{"Pesos:"} \n{int(vuelto)}")
+    print(f"{"Centavos:"} \n{float(vuelto) - int(vuelto)}")
 
 change()
